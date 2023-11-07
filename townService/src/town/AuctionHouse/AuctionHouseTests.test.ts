de03be2e3305d07c6ca1bffa08bf869e453392dc
JackGitter <gitter.j@northeastern.edu>
2023-11-07 16:49:03 -0500

    another test down

--- townService/src/town/AuctionHouse/AuctionHouseTests.test.ts
+++ townService/src/town/AuctionHouse/AuctionHouseTests.test.ts
@@ -218,8 +218,12 @@ describe('when joining an auction floor', () => {
 
 describe('when an auction floor ends', () => {
   let testArtwork: Artwork;
+  let testArtworkIsNotBeingAuctioned: Artwork;
   let testArtwork2: Artwork;
   let testArtworkIsBeingAuctioned: Artwork;
+  let testArtwork2IsBeingAuctioned: Artwork;
+  let testArtwork2IsNotBeingAuctioned: Artwork;
+
   beforeEach(() => {
     testArtwork = {
       description: 'Its the Mona Lisa',
@@ -251,6 +255,21 @@ describe('when an auction floor ends', () => {
       isBeingAuctioned: true,
       purchaseHistory: [],
     };
+    testArtworkIsNotBeingAuctioned = {
+      description: 'Its the Mona Lisa',
+      id: 1,
+      primaryImage: 'monalisa.png',
+      purchasePrice: 500000,
+      department: 'unknown',
+      title: 'The mona lisa',
+      culture: 'unknown',
+      period: '1500',
+      artist: { name: 'da Vinci' },
+      medium: 'Canvas',
+      countryOfOrigin: 'Italy',
+      isBeingAuctioned: false,
+      purchaseHistory: [],
+    };
     testArtwork2 = {
       description: 'Its stary night',
       id: 2,
@@ -264,6 +283,36 @@ describe('when an auction floor ends', () => {
       medium: 'Canvas',
       countryOfOrigin: 'France',
       isBeingAuctioned: false,
+      purchaseHistory: [],
+    };
+    testArtwork2IsBeingAuctioned = {
+      description: 'Its stary night',
+      id: 2,
+      primaryImage: 'starynight.png',
+      purchasePrice: 100000000000,
+      department: 'unknown',
+      title: 'Stary Night',
+      culture: 'unknown',
+      period: '1800',
+      artist: { name: 'Van Gogh' },
+      medium: 'Canvas',
+      countryOfOrigin: 'France',
+      isBeingAuctioned: true,
+      purchaseHistory: [],
+    };
+    testArtwork2IsNotBeingAuctioned = {
+      description: 'Its stary night',
+      id: 2,
+      primaryImage: 'starynight.png',
+      purchasePrice: 100000000000,
+      department: 'unknown',
+      title: 'Stary Night',
+      culture: 'unknown',
+      period: '1800',
+      artist: { name: 'Van Gogh' },
+      medium: 'Canvas',
+      countryOfOrigin: 'France',
+      isBeingAuctioned: true,
       purchaseHistory: [],
     };
   });
@@ -282,6 +331,33 @@ describe('when an auction floor ends', () => {
       expect(house.auctionFloors[0].artBeingAuctioned).toEqual(testArtworkIsBeingAuctioned);
       expect(house.auctionFloors[0].status).toEqual('WAITING_TO_START');
       expect(house.auctionFloors[0].timeLeft).toBe(30);
+      await dao.removeAuctionHouse();
+      await dao.removeArtworkIDList();
+    }, 100000);
+    it('does give artwork to player when their bid is the highest at the timer end', async () => {
+      const player = new Player(nanoid(), mock<TownEmitter>());
+      player.initializeArtAuctionAccount('player@gmail.com');
+      await dao.addPlayer(player.email);
+
+      const house = new AuctionHouse(nanoid(), testAreaBox, mock<TownEmitter>());
+      await house.addArtworksToAuctionHouse([testArtwork, testArtwork2]);
+      await house.createNewAuctionFloorNonPlayer();
+      house.auctionFloors[0].timeLeft = 1;
+      house.auctionFloors[0].currentBid = { player, bid: 500000 };
+      house.auctionFloors[0].startAuction();
+
+      // eslint-disable-next-line no-promise-executor-return
+      await new Promise(res => setTimeout(res, 2000));
+
+      // expect(player.artwork).toEqual([testArtwork]);
+      const playerResponse = await dao.getPlayer(player.email);
+      const { artworks } = playerResponse;
+      expect(artworks).toEqual([testArtworkIsNotBeingAuctioned]);
+
+      expect(house.auctionFloors).toHaveLength(1);
+      expect(house.auctionFloors[0].artBeingAuctioned).toEqual(testArtwork2IsBeingAuctioned);
+
+      await dao.removePlayer(player.email);
       await dao.removeAuctionHouse();
       await dao.removeArtworkIDList();
     }, 100000);
