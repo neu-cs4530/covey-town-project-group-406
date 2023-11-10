86687ce5b43fcbd22dd864a319cd74eb2f9bfa45
JackGitter <gitter.j@northeastern.edu>
2023-11-10 14:40:40 -0500

    rearanged imports for debug

--- frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
+++ frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
 import LogoutButton from './LogoutButton';
+import SignupForm from './SignupForm';
 
 export default function AuctionHouseLogin(): JSX.Element {
   // if the user creates an account or tries to log in, then do that and send the sendLoginCommand so
