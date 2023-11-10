d7de609d0754cdfbe331a96d033dbb8b2d8cf9e7
JackGitter <gitter.j@northeastern.edu>
2023-11-10 12:18:45 -0500

    nice

--- frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
+++ frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
+import LogoutButton from './LogoutButton';
 
 export default function AuctionHouseLogin(): JSX.Element {
   // if the user creates an account or tries to log in, then do that and send the sendLoginCommand so
   // the server can update the database value
   return (
     <>
-      <SignupForm />;
+      <SignupForm />
       <LoginForm />
+      <LogoutButton />
     </>
   );
 }
