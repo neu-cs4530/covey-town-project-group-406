0b0d03b9a2133dd05e11d6c59c1d400c2f90c795
JackGitter <gitter.j@northeastern.edu>
2023-11-09 20:07:32 -0500

    testing if a dummy user is created

--- frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
+++ frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
@@ -1,11 +1,15 @@
 import useTownController from '../../hooks/useTownController';
 import React from 'react';
+import auth from '../../classes/FirestoreConfig';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 export default function AuctionHouseLogin(): JSX.Element {
   const townController = useTownController();
   townController.addListener('loginStatus', () => {
     console.log('user logged in!');
   });
   const sendLoginCommand = () => {
+    createUserWithEmailAndPassword(auth, 'dummy@gmail.com', 'bs');
     townController.sendLoginCommand();
   };
   return <button onClick={sendLoginCommand}>Login</button>;
