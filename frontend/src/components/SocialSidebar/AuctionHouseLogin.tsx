f29b6a81a6715bdf538cd83b7829b9d251d3199b
JackGitter <gitter.j@northeastern.edu>
2023-11-09 20:24:29 -0500

    super nice

--- frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
+++ frontend/src/components/SocialSidebar/AuctionHouseLogin.tsx
@@ -2,23 +2,10 @@ import useTownController from '../../hooks/useTownController';
 import React from 'react';
 import auth from '../../classes/FirestoreConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import LoginForm from './LoginForm';
 
 export default function AuctionHouseLogin(): JSX.Element {
-  const townController = useTownController();
-  townController.addListener('loginStatus', () => {
-    console.log('user logged in!');
-  });
   // if the user creates an account or tries to log in, then do that and send the sendLoginCommand so
   // the server can update the database value
-  const sendLoginCommand = () => {
-    try {
-      createUserWithEmailAndPassword(auth, 'dummy@gmail.com', 'bsstopthatbullshit');
-    } catch (err) {
-      if (err instanceof Error) {
-        console.log(err.message);
-      }
-    }
-    townController.sendLoginCommand();
-  };
-  return <button onClick={sendLoginCommand}>Login</button>;
+  return <LoginForm />;
 }
