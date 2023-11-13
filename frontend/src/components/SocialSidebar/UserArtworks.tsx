dee5c87364a90e7cd2cdcf8ad321d80576cb9703
JackGitter <gitter.j@northeastern.edu>
2023-11-13 13:11:45 -0500

    working

--- frontend/src/components/SocialSidebar/UserArtworks.tsx
+++ frontend/src/components/SocialSidebar/UserArtworks.tsx
@@ -1,24 +1,31 @@
 import React from "react"
 import { Artwork } from "../../types/CoveyTownSocket"
-import { Box, Heading, Image } from "@chakra-ui/react"
+import { Box, Heading, Image, StackDivider, VStack } from "@chakra-ui/react"
 type Props = {
     userArtworks: Artwork[]
   }
 export default function UserArtworks({userArtworks}: Props): JSX.Element {
-
-
-
     return (
-        <Box>
+    <VStack
+      align='left'
+      spacing={2}
+      border='2px'
+      padding={2}
+      marginLeft={2}
+      borderColor='gray.500'
+      height='100%'
+      divider={<StackDivider borderColor='gray.200' />}
+      borderRadius='4px'>
+
         {userArtworks.map((artwork: Artwork) => {
             return (
             <Box>
                 <Heading as='h2' fontSize='lg'>{artwork.id}</Heading>
                 <Image src={artwork.primaryImage} alt="not found"></Image>
             </Box>
-            )
-        }
+            )}
         )}
-        </Box>
+
+    </VStack>
     )
 }
