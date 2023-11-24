1b1c7f7f4bd36e515fd748ff9cc9608e35742bca
JackGitter <gitter.j@northeastern.edu>
2023-11-23 22:42:34 -0500

    nice

--- townService/src/town/AuctionHouse/AuctionHouse.ts
+++ townService/src/town/AuctionHouse/AuctionHouse.ts
@@ -70,6 +70,9 @@ export default class AuctionHouse extends InteractableArea {
     for (const floor of this._auctionFloors) {
       floor.bidders = floor.bidders.filter(b => b.id !== player.id);
       floor.observers = floor.observers.filter(o => o.id !== player.id);
+      if (floor.auctioneer?.id === player.id) {
+        this._deleteAuctionFloor(floor.id);
+      }
     }
     this._emitAreaChanged();
   }
