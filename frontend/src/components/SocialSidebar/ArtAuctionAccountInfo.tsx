ba631d3854fa52a087ec6427df70661c79dde1a5
Riddhi Gupta <riddhig566@gmail.com>
2023-11-25 16:51:04 -0500

    renamed file

--- frontend/src/components/SocialSidebar/ArtAuctionAccountInfo.tsx
+++ frontend/src/components/SocialSidebar/ArtAuctionAccountInfo.tsx
@@ -14,7 +14,7 @@ import {
 } from '@chakra-ui/react';
 import { Artwork } from '../../types/CoveyTownSocket';
 import { Typography } from '@material-ui/core';
-import UserArtwork from './UserArtworks';
+import UserArtwork from './UserArtwork';
 
 type Props = {
   userEmail: string;
